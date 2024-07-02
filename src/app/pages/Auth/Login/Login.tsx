709649b7f8aef21e999e01c971c79f77f5f56a68
Manny <officialeminz@gmail.com>
2024-07-01 21:01:15 -0700

    ui: login page completed

--- src/app/pages/Auth/Login/Login.tsx
+++ src/app/pages/Auth/Login/Login.tsx
@@ -1,8 +1,85 @@
 import { FC } from "react";
+import { LoginContainer } from "./LoginContainer";
+import FormInput from "../../../../components/ui/FormInput";
+import AppButton from "../../../../components/ui/Button";
+import { Link } from "react-router-dom";
+import { Form } from "react-bootstrap";
 
 export interface LoginProps {}
 const Login: FC<LoginProps> = () => {
-  return <div>Login</div>;
+  return (
+    <LoginContainer
+      maintitle={
+        <>
+          <span className="fw-normal">Welcome Back</span>
+        </>
+      }
+      subtitle={
+        <>
+          Choose <span className="fw-normal">the</span> double Option{" "}
+          <span className="fw-normal">and </span> Get Started
+        </>
+      }
+      label={
+        <>
+          <span className="fw-bold">
+            Are you ready to achieve your financial goals and grow wealth with
+            our secure options for naria and dollar savings investments.
+          </span>
+        </>
+      }
+      testimony={
+        <>
+          <div>
+            <h4
+              className="text-secondary fw-lighter my-3"
+              style={{ fontSize: "18px" }}
+            >
+              I can't believe how much peace of mind Doubble has given me. Thank
+              you for helping me build a net and securing my financial future!.
+            </h4>
+            <h4 className="text-secondary fs-5">Stella O.</h4>
+          </div>
+        </>
+      }
+    >
+      <div className="px-5">
+        <FormInput type="email" placeholder="Email Address" />
+        <FormInput type="password" placeholder="Password" />
+        <div className="d-flex align-items-center justify-content-between">
+          <Form.Check type="checkbox" id="custom-switch" label="Remember me" />
+          <div className="text-primary">Forgot Password?</div>
+        </div>
+        <AppButton
+          variant="info"
+          size="lg"
+          fullWidth
+          className="mt-4"
+          onClick={() => {}}
+        >
+          Login
+        </AppButton>
+        <div className="text-secondary mt-4 d-flex align-items-center justify-content-center">
+          Don't have an account?{" "}
+          <Link to={"/login"}>
+            <span
+              className="text-accent ms-1 fw-bold"
+              style={{ textDecoration: "none !important" }}
+            >
+              Sign up
+            </span>
+          </Link>
+        </div>
+        <div className="text-secondary mt-4 d-flex flex-column mt-5 align-items-center justify-content-center">
+          <div className="text-secondary">By signing up you agree to our</div>
+          <div className="text-accent">
+            Terms and Conditions <span className="text-secondary">and</span>{" "}
+            Privacy Statement
+          </div>
+        </div>
+      </div>
+    </LoginContainer>
+  );
 };
 
 export default Login;
--- src/app/pages/Auth/Login/LoginContainer/LoginContainer.tsx
+++ src/app/pages/Auth/Login/LoginContainer/LoginContainer.tsx
@@ -1,6 +1,37 @@
-import { FC } from "react";
+import { FC, ReactNode } from "react";
+import { Card } from "react-bootstrap";
 
-export interface LoginContainerProps {}
-export const LoginContainer: FC<LoginContainerProps> = () => {
-  return <div>LoginContainer</div>;
+export interface LoginContainerProps {
+  children: ReactNode;
+  maintitle: ReactNode | string;
+  subtitle: ReactNode | string;
+  label: ReactNode | string;
+  testimony: ReactNode | string;
+}
+export const LoginContainer: FC<LoginContainerProps> = ({
+  children,
+  maintitle,
+  subtitle,
+  label,
+  testimony,
+}) => {
+  return (
+    <div className="d-flex flex-column align-items-center">
+      <div className="text-primary fs-2 fw-normal">{maintitle}</div>
+      <Card
+        className="bg-white rounded-5 border-0 mt-4 d-flex flex-row align-items-start"
+        style={{ width: "1100px", height: "440px", padding: "40px" }}
+      >
+        <div
+          className="d-flex flex-column align-items-start justify-content-center"
+          style={{ width: "40%" }}
+        >
+          <div className="text-primary fw-bold fs-3 mb-3">{subtitle}</div>
+          <div className="text-secondary">{label}</div>
+          <div className="text-secondary">{testimony}</div>
+        </div>
+        <div style={{ width: "60%" }}>{children}</div>
+      </Card>
+    </div>
+  );
 };
